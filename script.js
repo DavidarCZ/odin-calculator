d5ea70a62a156c039ee25bf2521a867ecc5d8f87
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-19 21:45:55 +0100

    Implement operate function
    Implemented operate function that decides what sign
    function to call. Also Added sections in script.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//////////////////////////////////////////////////
+//Functions
+//////////////////////////////////////////////////
 function add(first, second)
 {
     return first + second;
@@ -17,6 +20,26 @@ function divide(first, second)
 {
     return first/second;
 }
+
+function operate(first, op, second)
+{
+    switch (op)
+    {
+        case '+':
+            return add(first,second);
+        case '-':
+            return subtract(first,second);
+        case '*':
+            return multiply(first,second);
+        case '/':
+            return divide(first,second);  
+        default:
+            return null;
+    }
+}
+//////////////////////////////////////////////////
+//Variables
+//////////////////////////////////////////////////
 
 let firstNumber;
 let operator;
